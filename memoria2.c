1e79cccedc28b6ed083f03243acd85d7e8f62ab5
Hugo01 <hugocortes01@gmail.com>
2018-11-23 14:25:50 -0200

    Codigo rodando, sem deadlock, falta destruir semaforos

--- memoria2.c
+++ memoria2.c
@@ -8,13 +8,14 @@
 #include<sys/ipc.h>
 #include<sys/shm.h>
 #include<sys/sem.h>
+#include<semaphore.h>
+#include<fcntl.h>
 
-struct sembuf operacao[2];
+struct sembuf operacao[1];
 int idsem;
-int i;
 
-void p_sem();
-void v_sem();
+void p_sem1();
+void v_sem1();
 void p_sem2();
 void v_sem2();
 void destroi_semaforo(int id);
@@ -23,20 +24,17 @@ void destroi_semaforo(int id);
 void p_sem2 ()
 {
   operacao[0].sem_num = 0;
-  operacao[0].sem_op = 0;
-  operacao[0].sem_flg = 0;
-  operacao[1].sem_num = 0;
-  operacao[1].sem_op = 1;
-  operacao[1].sem_flg = 0;
-  if (semop (idsem, operacao, 2) < 0)
+  operacao[0].sem_op = -1;
+  operacao[0].sem_flg = SEM_UNDO;
+  if (semop (idsem, operacao, 1) < 0)
     printf ("erro P2 no p=%d\n", errno);
 }
 
 void v_sem2 ()
 {
   operacao[0].sem_num = 0;
-  operacao[0].sem_op = -1;
-  operacao[0].sem_flg = 0;
+  operacao[0].sem_op = 1;
+  operacao[0].sem_flg = SEM_UNDO;
   if (semop (idsem, operacao, 1) < 0)
     printf ("erro V2 no p=%d\n", errno);
 }
@@ -44,12 +42,9 @@ void v_sem2 ()
 void p_sem1 ()
 {
   operacao[0].sem_num = 1;
-  operacao[0].sem_op = 0;
-  operacao[0].sem_flg = 0;
-  operacao[1].sem_num = 1;
-  operacao[1].sem_op = 1;
-  operacao[1].sem_flg = 0;
-  if (semop (idsem, operacao, 2) < 0)
+  operacao[0].sem_op = -1;
+  operacao[0].sem_flg = SEM_UNDO;
+  if (semop (idsem, operacao, 1) < 0)
     printf ("erro P1 no p=%d\n", errno);
    // printf("idsem: %d\n", idsem);
 }
@@ -57,79 +52,83 @@ void p_sem1 ()
 void v_sem1 ()
 {
   operacao[0].sem_num = 1;
-  operacao[0].sem_op = -1;
-  operacao[0].sem_flg = 0;
+  operacao[0].sem_op = 1;
+  operacao[0].sem_flg = SEM_UNDO;
   if (semop (idsem, operacao, 1) < 0)
     printf ("erro V1 no p=%d\n", errno);
 }
 
 int main ()
 {
-
   int pid, estado, idshm;
   struct shmid_ds buf;
   int *pshm;
   int *psem;
+  int i, j=0;
 
 /* cria memoria */
-if ((idshm = shmget(0x1223, sizeof(int),IPC_CREAT|0x1ff)) < 0)
+if ((idshm = shmget(0x1223, sizeof(int), IPC_CREAT|0x1ff)) < 0)
   { 
     printf("erro na criação da fila\n"); 
     exit(1);
   }
 
 /* cria semaforo */
-if ((idsem = semget (0x1111, 2, IPC_CREAT | 0x1ff)) < 0)
+if ((idsem = semget (0x1111, 2, IPC_CREAT|0x1ff)) < 0)
   {
-    printf ("erro na criacao do semaforo\n");
+    printf ("erro na criação do semáforo\n");
     exit (1);
   }
 
-/* codigo do filho */
-pid = fork();
-if (pid != 0)
-  {
-    for(i = 0 ; i <= 19 ; i++)
+pshm = (int *) shmat(idshm, (char *)0, 0);
+if (pshm == (int *) -1)
 	{
-      pshm = (int *) shmat(idshm, (char *)0, 0);
-	  if (pshm == (int *)-1)
-	  {
-		printf("erro no attach\n"); 
-        exit(1);
-	  }else {
-	  v_sem1();
-      printf("filho - obtive o semaforo\n");
-      printf("filho - recebi do pai %d\n", *pshm);
-      p_sem2();
-      }
-    }
-  }	
-
-/* codigo do pai */
-for(i = 0 ; i <= 19 ; i++)
-  {
-	pshm = (int *) shmat(idshm, (char *)0, 0);  
-	if (pshm == (int *) -1)
-	{
-      printf("erro no attach\n"); 
-      exit(1);
-	}	
-    *pshm = i;
-    printf("PAI - registrei um novo número\n");  
-    printf("PAI - escrevi %d para o filho\n", *pshm);
-    p_sem1();
-    v_sem2();
+    printf("erro no attach\n"); 
+    exit(1);
   }
 
 
-//wait(&estado);
- 
+/* codigo do filho */
+pid = fork();
+if (pid < 0)
+	printf("Fork error.\n");
+if (pid != 0)
+  {
+    for(i = 0 ; i <= 19 ; i++)
+	  {
+      p_sem1();
+      printf("filho - obtive o semáforo\n");
+      printf("filho - recebi do pai %d\n", *pshm);
+      v_sem2();
+    }
+  }	
+/* codigo do pai */
+if (i < 19)
+{
+for(i = 0 ; i <= 19 ; i++)
+{  
+	*pshm = i;
+  printf("PAI - registrei um novo número\n");  
+  printf("PAI - escrevi %d para o filho\n", *pshm);
+  v_sem1();
+  p_sem2();
+}   
+}else
+{
+  printf("SAI\n");
+}
 
+sleep(5);
 
+printf("adeus\n");
+shmctl (idshm, IPC_RMID, 0);
+shmctl (idsem, IPC_RMID, 0);
+
+/*
 if(wait(&estado) == 0){
 destroi_semaforo(idsem);
 }
-
+*/
 
 exit (0);
 }
